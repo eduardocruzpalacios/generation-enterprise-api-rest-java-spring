f5ba35cfdde448e86d0cb25266d00617ca54bc13
educp <52535457+eduardocruzpalacios@users.noreply.github.com>
2021-05-11 09:46:02 +0200

    inicio proyecto

---
+++ src/main/java/com/ejemplos/spring/GenerationEnterpriseRestApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplos.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerationEnterpriseRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerationEnterpriseRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejemplos/spring/GenerationEnterpriseRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplos.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerationEnterpriseRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
