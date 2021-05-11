329556ecea3acbf877df0b2d2d568403ccacd293
unknown <80522547+Arrshe@users.noreply.github.com>
2021-05-11 14:13:13 +0200

    metodo findall en capa service y test findall de la capa service

---
+++ src/test/java/com/ejemplos/spring/Test_findAll_service.java
@@ -0,0 +1,37 @@
+/*
+ * @Author Maria
+ * @Version 1.0
+ */
+
+package com.ejemplos.spring;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ejemplos.spring.service.ProyectoServiceImpl;
+
+@SpringBootTest
+public class Test_findAll_service {
+ 
+	@Autowired
+	private ProyectoServiceImpl proyectoServiceImpl;
+	
+	@Test
+	void existe() {
+		assertThat(proyectoServiceImpl).isNotNull();
+	}
+	
+	@Test
+	void noDevuelveNada() {
+		assertThat(proyectoServiceImpl.findAll()).isNull();
+	}
+	
+	@Test
+	void devuelveCosas() {
+		assertThat(proyectoServiceImpl.findAll()).isNotNull();
+	}
+	
+}
