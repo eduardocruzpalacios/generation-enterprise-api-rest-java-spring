17c66798dca7b25eda08b0ac5ec455ddb3521434
educp <52535457+eduardocruzpalacios@users.noreply.github.com>
2021-05-12 11:21:05 +0200

    main cambios

--- src/main/java/com/ejemplos/spring/GenerationEnterpriseRestApplication.java
+++ src/main/java/com/ejemplos/spring/GenerationEnterpriseRestApplication.java
@@ -1,13 +1,30 @@
 package com.ejemplos.spring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+import com.ejemplos.spring.model.Proyecto;
 
 @SpringBootApplication
-public class GenerationEnterpriseRestApplication {
+public class GenerationEnterpriseRestApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GenerationEnterpriseRestApplication.class, args);
+		// SpringApplication.run(GenerationEnterpriseRestApplication.class, args);
+		SpringApplication app = new SpringApplication(GenerationEnterpriseRestApplication.class);
+		// app.setWebEnvironment(false); // no entiendo este error, he seguido
+		// https://www.arquitecturajava.com/spring-rest-client-con-resttemplates/
+		app.run(args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+
+		RestTemplate plantilla = new RestTemplate();
+		Proyecto listarProyectos = plantilla.getForObject("http://localhost:8080/listar", Proyecto.class);
+		System.out.println(listarProyectos);
+
 	}
 
 }
