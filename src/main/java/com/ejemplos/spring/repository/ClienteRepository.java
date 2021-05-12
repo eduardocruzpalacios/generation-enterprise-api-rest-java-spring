ccfa33e2de817b5ecc521fb45a31e41ff1bf66ca
unknown <80522547+Arrshe@users.noreply.github.com>
2021-05-12 13:25:51 +0200

    he tocas cosas de la capa repo

--- src/main/java/com/ejemplos/spring/repository/ClienteRepository.java
+++ src/main/java/com/ejemplos/spring/repository/ClienteRepository.java
@@ -8,9 +8,7 @@
 package com.ejemplos.spring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ClienteRepository<Cliente> extends JpaRepository<Cliente, Integer> {
 
 }
