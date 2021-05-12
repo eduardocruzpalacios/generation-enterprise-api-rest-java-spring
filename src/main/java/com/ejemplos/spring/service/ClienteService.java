26241c08156c64e613317c8879edf519c0397e54
unknown <80522547+Arrshe@users.noreply.github.com>
2021-05-12 10:18:33 +0200

    metodo findAll para listar clientes en la capa service

---
+++ src/main/java/com/ejemplos/spring/service/ClienteService.java
@@ -0,0 +1,10 @@
+package com.ejemplos.spring.service;
+
+import java.util.List;
+import com.ejemplos.spring.model.Cliente;
+
+public interface ClienteService {
+
+	public List<Cliente> findAll();
+
+}
