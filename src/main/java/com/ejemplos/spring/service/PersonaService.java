21580be5fc1efc5bc80fb01641742e1a43d5c53f
educp <52535457+eduardocruzpalacios@users.noreply.github.com>
2021-05-13 12:03:28 +0200

    método findAll():List<Persona> en capa Service

--- src/main/java/com/ejemplos/spring/service/PersonaService.java
+++ src/main/java/com/ejemplos/spring/service/PersonaService.java
@@ -1,5 +1,15 @@
+/*
+ * 13 mayo 2021
+ */
 package com.ejemplos.spring.service;
 
+import java.util.List;
+
+import com.ejemplos.spring.model.Persona;
+
 public interface PersonaService {
+
+	// metodo para listar personas
+	public List<Persona> findAll();
 
 }
--- src/main/java/com/ejemplos/spring/service/PersonaServiceImpl.java
+++ src/main/java/com/ejemplos/spring/service/PersonaServiceImpl.java
@@ -1,5 +1,24 @@
+/*
+ * 13 mayo 2021
+ */
 package com.ejemplos.spring.service;
 
-public class PersonaServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ejemplos.spring.model.Persona;
+import com.ejemplos.spring.repository.PersonaRepository;
+
+@Service
+public class PersonaServiceImpl implements PersonaService {
+
+	@Autowired
+	PersonaRepository repo;
+
+	// metodo para listar personas
+	public List<Persona> findAll() {
+		return repo.findAll();
+	}
 }
