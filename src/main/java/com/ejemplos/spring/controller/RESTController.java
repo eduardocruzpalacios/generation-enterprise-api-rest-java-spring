04a486b1da1284631ce2a465ae3d0bc1a24b2ad7
Raul <80522684+Raul1551@users.noreply.github.com>
2021-05-17 12:54:29 +0200

    Cambios en capa controller REST, pruebas realizadas, funciona

--- src/main/java/com/ejemplos/spring/controller/RESTController.java
+++ src/main/java/com/ejemplos/spring/controller/RESTController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -59,9 +60,9 @@ public class RESTController {
 		proyectoService.deleteById(idproyecto);
 	}
 	
-	@GetMapping("/editarproyecto/{id}")
-	public void editarProyecto(@PathVariable("id")Integer idproyecto) {
-		proyectoService.findById(idproyecto);
+	@PutMapping("/editarproyecto")
+	public void editarProyecto(@RequestBody Proyecto p) {
+		proyectoService.save(p);
 	}
 
 	@GetMapping("/equipo") // página del equipo
