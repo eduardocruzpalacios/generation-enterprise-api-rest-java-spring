0149923457fa566b7dd71066f11c8d44f0208373
Miguel <80522776+migdom7@users.noreply.github.com>
2021-05-19 19:45:22 +0200

    cambios en clase persona

--- src/main/java/com/ejemplos/spring/model/Persona.java
+++ src/main/java/com/ejemplos/spring/model/Persona.java
@@ -14,6 +14,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -33,7 +34,7 @@ public class Persona {
 
 	private String foto;
 
-	@OneToOne
+	@ManyToOne
 	@JoinColumn(name = "cargo")
 	private Cargo cargo;
 
